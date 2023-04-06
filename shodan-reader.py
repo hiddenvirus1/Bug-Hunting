f8bc314785e6c5f5d334408ef16d505d9d196c35
Hidden-Virus <70848309+hiddenvirus1@users.noreply.github.com>
2023-04-07 03:20:59 +0500

    Update shodan-reader.py

--- shodan-reader.py
+++ shodan-reader.py
@@ -4,6 +4,7 @@ import jsonlines
 # Parse the command-line arguments
 parser = argparse.ArgumentParser(description='Read Shodan results from a JSON file.')
 parser.add_argument('-f', '--filename', required=True, help='Path to the input file')
+parser.add_argument('-ip', '--only-ip', action='store_true', help='Print only the IP addresses')
 args = parser.parse_args()
 
 # Open the file and read the JSON data
@@ -12,29 +13,34 @@ with open(args.filename, 'r') as f:
 
     # Loop through each result in the JSON data
     for result in results:
-        # Extract the IP address, hostname, open ports, status, and organization name from the result
+        # Extract the IP address from the result
         ip_address = result['ip_str']
-        hostnames = result.get('hostnames', [])
-        domains = result.get('domains', [])
-        open_ports = result.get('port', [])
-        if not isinstance(open_ports, list):
-            open_ports = [open_ports]  # Convert to a list if not already a list
-        status = result.get('http', {}).get('status', '') or result.get('https', {}).get('status', '')
-        org_name = result.get('org', '')
-        
 
-        # Print the data to the console
-        print(f"IP Address: {ip_address}")
-        if hostnames:
-            print(f"Hostnames: {', '.join(hostnames)}")
-        if domains:
-            print(f"Domains: {', '.join(domains)}")
-        if open_ports:
-            print(f"Open ports list: {', '.join(map(str, open_ports))}")
-        if status:
-            print(f"Status: {status}")
-        if org_name:
-            print(f"Organization: {org_name}")
+        # Print only the IP address if specified by the user
+        if args.ip:
+            print(ip_address)
+        else:
+            # Extract the other fields if not printing only the IP addresses
+            hostnames = result.get('hostnames', [])
+            domains = result.get('domains', [])
+            open_ports = result.get('port', [])
+            if not isinstance(open_ports, list):
+                open_ports = [open_ports]  # Convert to a list if not already a list
+            status = result.get('http', {}).get('status', '') or result.get('https', {}).get('status', '')
+            org_name = result.get('org', '')
+            
+            # Print the data to the console
+            print(f"IP Address: {ip_address}")
+            if hostnames:
+                print(f"Hostnames: {', '.join(hostnames)}")
+            if domains:
+                print(f"Domains: {', '.join(domains)}")
+            if open_ports:
+                print(f"Open ports: {', '.join(map(str, open_ports))}")
+            if status:
+                print(f"Status: {status}")
+            if org_name:
+                print(f"Organization: {org_name}")
 
-        # Print a separator line between each result
-        print("=====================================")
+            # Print a separator line between each result
+            print("=====================================")
